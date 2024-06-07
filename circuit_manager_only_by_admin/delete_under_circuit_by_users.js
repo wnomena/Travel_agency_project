98b4278785449d116008a48d6cd546216714b9c5
wnomena <wnomena58@gmail.com>
2024-06-07 21:58:48 +0300

    route_restant

---
+++ circuit_manager_only_by_admin/delete_under_circuit_by_users.js
@@ -0,0 +1,23 @@
+module.exports = (app,child_road_model) =>{
+    app.delete("/utilisateurs/delete/child_road/:name",(req,res)=>{
+        if(req.params.name == undefined || req.params.name == ""){
+            const message = "Veuillez vérifier l'élément à supprimer"
+            return res.stauts(400).json({message})
+        }
+        try {
+            child_road_model.find({name : btoa(req.params.name)}).then((a)=>{
+                if(a == "" || a == undefined){
+                    const message = "Veuillez vérifier l'élément à supprimer"
+                return res.stauts(400).json({message})
+                }
+                child_road_model.deleteOne({name : btoa(req.params.name)}).then((b)=>{
+                    const message = "Action effectuer avec succès"
+                    return res.json({message})
+                })
+            })
+        } catch (error) {
+            const message = "Le serveur ne répon pas, veuillez réessayer  ultérieurement"
+            return res.status(500).json({message,error})
+        }
+    })
+}
---
+++ circuit_manager_only_by_admin/update_parent_road_information.js
@@ -0,0 +1,5 @@
+module.exports = (app,parent_road_model) =>{
+    app.put("/utilisateurs/update_/parent_road/information",(req,res)=>{
+        
+    })
+}
