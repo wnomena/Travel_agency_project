135ce2940291307b6ad293f811381c431cbd98e1
wnomena <wnomena58@gmail.com>
2024-09-16 11:05:52 +0300

    avant_hebergement

--- circuit_manager_only_by_admin/delete_under_circuit_by_users.js
+++ circuit_manager_only_by_admin/delete_under_circuit_by_users.js
@@ -1,3 +1,6 @@
+function find_one_and_unlink(name) {
+    fs.unlink(`./file/${name}`,(err) => console.log(err))
+}
 const fs = require("fs")
 module.exports = (app,child_road_model) =>{
     app.delete("/utilisateurs/delete/child_road/:name",(req,res)=>{
@@ -8,10 +11,11 @@ module.exports = (app,child_road_model) =>{
         try {
             child_road_model.find({name : btoa(req.params.name)}).then((a)=>{
                 if(a == "" || a == undefined){
-                    const message = "Veuillez vérifier l'élément à supprimer"
+                const message = "Veuillez vérifier l'élément à supprimer"
                 return res.stauts(400).json({message})
                 }
                 child_road_model.deleteOne({name : btoa(req.params.name)}).then((b)=>{
+                    find_one_and_unlink(a[0].presentation_image.split("/")[a[0].presentation_image.split("/").length - 1])
                     const message = "Action effectuer avec succès"
                     return res.json({message})
                 })
--- circuit_manager_only_by_admin/update_child_way_by_admin.js
+++ circuit_manager_only_by_admin/update_child_way_by_admin.js
@@ -1,8 +1,31 @@
+const fs = require("fs")
 const multer = require("../multer_middleware")
 module.exports = (app,parent_road_model) =>{
-    app.put("/utilisateurs/update_child_way/by_user/:id",multer,(req,res)=>{
+    app.put("/utilisateurs/update_child_way/by_user",multer,(req,res)=>{
         const body = JSON.parse(req.body.body)
-        const array = [{name : "parent_ident_equal_to_child",value : body.parent_ident_equal_to_child},{name : "name",value : body.name},{name : "description",value : body.description},{name : "distance",value : body.distance},{name : "presentation_image",value : req.file.filename},{name : "sejour_delay",value : body.sejour_delay},{name : "price", value : body.price}]
+        const array = [{name : "parent_ident_equal_to_child",value : body.parent_ident_equal_to_child},{name : "name",value : body.name},{name : "description",value : body.description},{name : "distance",value : body.distance},{name : "presentation_image",value : req.file.filename},{name : "sejour_delay",value : body.sejour_delay},{name : "price", value : body.price},{name : "period",value: body.period}]
+        for( let i of array) if(!i.value) return res.status(400).json({message : "Vérifier toutes les informations que vous avez saisi"})
+        try {
+            parent_road_model.find({name : btoa(array[0].value)}).then((res) => {
+                if(res.length !== 0) {
+                    fs.unlink(`./file${Array.from(res[0].presentation_image.split("/"))[Array.from(res[0].presentation_image.split("/")).length -1]}`,(err) => console.log(err))
+                    parent_road_model.findByIdAndUpdate(res[0]._id,{
+                        description : btoa(body.description),
+                        distance : btoa(body.distance),
+                        presentation_image : `http://localhost:5000/get/${req.file.filename}`,
+                        sejours_delay : btoa(body.sejours_delay),
+                        price : btoa(body.price),
+                        difficulty : btoa(body.difficulty),
+                        confort : btoa(body.confort),
+                        period : btoa(body.period)
+                    }).then((resp) => {
+                        res.json({message : "Route modifier"})
+                    })
+                }
+            })
+        } catch (error) {
+            return res.status(500).json({message : error})
+        }
         //tohizana rahapitso
     })
 }
