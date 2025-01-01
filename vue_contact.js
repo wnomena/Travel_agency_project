f812a1ed6825e517be5085a191b20e1109fc2640
wnomena <wnomena58@gmail.com>
2025-01-01 19:59:19 +0300

    update_noum

--- vue_contact.js
+++ vue_contact.js
@@ -4,10 +4,11 @@ module.exports = (req,res) => {
     try {
         contact_model.find({name : req.body.name}).then((value) => {
             contact_model.findByIdAndUpdate(value._id,{seen : true}).then((re) => {
-                res.json({message : re})
+                const message = "Form updated"
+                res.json({message})
             })
         })
     } catch (error) {
-        res.status(500).json({message : error})
+        res.status(500).json({message : JSON.stringify(error)})
     }
 }
